adfec356876a2cc77c21c3ca55aa33e7a3a61838
alexis <37817243+adeane89@users.noreply.github.com>
2018-07-16 18:05:35 -0500

    Daily forecast with codes

--- weather.js
+++ weather.js
@@ -9,6 +9,11 @@ function onSubmitForm() {
 function weather(){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + placeEntered + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", (jsonData) => {
         $(".title").html(jsonData.query.results.channel.description);
-        $(".forecast").html(jsonData.query.results.channel.item.description);
+        //$(".forecast").html(jsonData.query.results.channel.item.description);
+
+        jsonData.query.results.channel.item.forecast.forEach(function(e, i, a) {
+            $("#results").append("<div class= \"card text-white mx-auto border-secondary mb-3\" style = \"width: auto; height: auto; align-items: center; background-color: #414749; display:inline-block;\"><img class=\"card-img-top\" src =\"http://l.yimg.com/a/i/us/we/52/" +e.code + ".gif\"><p class = \"card-title\">" + e.day +"</p><p class=\"card-body\">" + e.high +"</p><p class=\"card-body\">" + e.low + "</p></div>");
+        console.log(e.day, e.code, e.high, e.low);
+        });
     });
 }
