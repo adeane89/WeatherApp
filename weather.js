9e6af7cd20a1247756e2655b76251e674b789949
alexis <37817243+adeane89@users.noreply.github.com>
2018-07-16 20:36:27 -0500

    forecast on bottom with current condition

--- weather.js
+++ weather.js
@@ -8,11 +8,14 @@ function onSubmitForm() {
 
 function weather(){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + placeEntered + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", (jsonData) => {
-        $(".title").html(jsonData.query.results.channel.description);
-        //$(".forecast").html(jsonData.query.results.channel.item.description);
+        $(".current").html(jsonData.query.results.channel.item.condition.temp)
+        $(".currentCondition").html(jsonData.query.results.channel.item.condition.text);
+        console.log(jsonData.query.results.channel.item.condition.temp);
+        $(".forecast").html(jsonData.query.results.channel.location.city);
+        console.log(jsonData.query.results.channel.location.city);
 
         jsonData.query.results.channel.item.forecast.forEach(function(e, i, a) {
-            $("#results").append("<div class= \"card text-white mx-auto border-secondary mb-3\" style = \"width: auto; height: auto; align-items: center; background-color: #414749; display:inline-block;\"><img class=\"card-img-top\" src =\"http://l.yimg.com/a/i/us/we/52/" +e.code + ".gif\"><p class = \"card-title\">" + e.day +"</p><p class=\"card-body\">" + e.high +"</p><p class=\"card-body\">" + e.low + "</p></div>");
+            $("#results").append("<div class=\"card text-white mx-auto\" style = \"width: auto; height: 0; align-items: center; display:inline-block; border: 0;\"><p class = \"card-title\"><p>" + e.day + "</p><img class=\"card-img-top-sm\" src =\"http://l.yimg.com/a/i/us/we/52/" + e.code + ".gif\"></p><p class=\"card-body\">" + e.high + "<span id=\"add_here\">&deg; /</span>" +  e.low + "<span id=\"add_here\">&deg;</span></p></div>");
         console.log(e.day, e.code, e.high, e.low);
         });
     });
